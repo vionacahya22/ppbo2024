9f5d5cbc7898a88805f35a1936ef6ef5a2db8366
vionachyaaul <vionaviona243@gmail.com>
2024-09-03 23:09:39 +0700

    Praktikum 4A - Membuat Class

--- inputoutput.php
+++ inputoutput.php
@@ -1,3 +1,5 @@
 <?php
+date_default_timezone_set("Asia/Jakarta");
 $nama = readline('Masukkan nama Anda: ');
-echo "Halo {$nama}";
+$waktu = date('H');
+echo "Halo {$nama} sekarang pukul {$waktu}\n";
