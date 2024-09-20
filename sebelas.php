bd536773b6c99b994131e5dd712334ed4b2020ed
vionachyaaul <vionaviona243@gmail.com>
2024-09-20 10:37:54 +0700

    Praktikum 6F - Membuat Objek Pewarisan

---
+++ sebelas.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Admin\Dosen;
+
+
+$dian = new Dosen(
+    "198411132015041001",  
+    "Dian Prawira",      
+    "62111111",            
+    "Jln Purnama",       
+    "0013118405"         
+);
+
+
+$dian->mengajar();
+?>
