1325d71a728a4262507987dfef13be9f62d46f23
vionachyaaul <vionaviona243@gmail.com>
2024-09-28 04:50:39 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- sebelas.php
+++ sebelas.php
@@ -1,18 +1,14 @@
 <?php
 
-require_once 'vendor/autoload.php';
+
 
 use App\Model\Akademik\Dosen;
 
+require_once 'vendor/autoload.php';
 
-$dian = new Dosen(
-    "198411132015041001",  
-    "Dian Prawira",      
-    "62111111",            
-    "Jln Purnama",       
-    "0013118405"         
-);
-
+$dian = new Dosen(198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", 
+"0013118405");
 
 $dian->mengajar();
-?>
+
+echo $dian->getNoHp();
